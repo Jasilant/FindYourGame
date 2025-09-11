f8588b24b50f8ee9672181d3380de098e6526862
Jasilant <jason.jasilant@gmail.com>
2025-09-11 12:00:27 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import './styles/globals.css';
-import type { ReactNode } from 'react';
+- import './styles/globals.css';
++ import '../styles/globals.css';
+
 
 export const metadata = {
   title: 'FindYourGame.ch',
