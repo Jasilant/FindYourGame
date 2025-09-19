661a747fa130a943c4f62ebe28eaf11423a9cd5a
Dein Name <deine@mailadresse.tld>
2025-09-19 16:26:45 +0200

    feat(brand): black-badge coarse pixel fox + set as favicon and navbar mark

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -125,7 +125,7 @@ export default function Navbar() {
           {/* BRAND */}
           <Link href="/" className="group flex items-center gap-2">
             <img
-              src="/logo-fox-pixel.svg"
+              src="/logo-fox-pixel-coarse.svg"
               alt=""
               className="h-7 w-7 transition-transform duration-200 group-hover:-translate-y-[1px] group-active:scale-95"
             />
